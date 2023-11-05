f0565b1fba7f5b4dd1c29a17860ece23a2b24ba8
Eric_Gdf <ericgodefroy@outlook.fr>
2023-11-05 18:53:12 +0100

    final fix

--- src/assets/components/Gift_list_section.jsx
+++ src/assets/components/Gift_list_section.jsx
@@ -4,7 +4,7 @@ import './gift_list_section.css'
 const Gift_list_section = () => {
   return (
     <div className='gift_background'>
-        <h2 className='section_title' id='gift'>Liste de mariage</h2>
+        <h2 className='section_title gift' id='gift'>Liste de mariage</h2>
         <a href="https://www.milirose.com/liste-cadeaux-613037.html" target="_blank">
         <button>Cliquez-ici pour voir notre Liste 🎁</button>
         </a>
